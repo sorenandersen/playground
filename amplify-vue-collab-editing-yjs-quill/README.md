c2caff572a4dddba4c40487cc76b93b1df33b5bd
Soren Andersen <mail@sorenandersen.com>
2021-03-05 15:08:03 +0100

    amplify-vue-collab scaffolding

---
+++ amplify-vue-collab-editing-yjs-quill/README.md
@@ -0,0 +1,25 @@
+# amplify-vue-collab-editing-yjs-quill
+
+## About
+
+Demo.
+
+## Vue project setup
+```
+npm install
+```
+
+### Developing, building, linting, ...
+```
+# Compiles and hot-reloads for development
+npm run serve
+
+# Compiles and minifies for production
+npm run build
+
+# Lints and fixes files
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ amplify-vue-collab-editing-yjs-quill/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <router-link :to="{ name: 'Home' }">
+        Home
+      </router-link>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+      DEMO
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({})
+};
+</script>
